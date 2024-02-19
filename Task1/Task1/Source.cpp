36a3bbdaca3b77c6b8178e9dd7b963f2bb266335
Sergey <sergejtiskovec311@gmail.com>
2024-02-19 18:12:25 +0300

    final version (I hope)

--- Task1/Task1/Source.cpp
+++ Task1/Task1/Source.cpp
@@ -1,54 +1,64 @@
 #include <iostream>
 #include <cmath>
-#include <limits>
 
 using namespace std;
 
 class quadratic_equation {
-	friend void solve_equation(quadratic_equation);
 private:
-	int a, b, c;
+	double a, b, c;
+	double roots[2];
+	int size;
 public:
 	quadratic_equation() {
-		cout << "Enter integer coefficients of quadratic equation by spaces: ";
+		cout << "Enter coefficients of quadratic equation by spaces: ";
 		cin >> a >> b >> c;
 	}
 
-	void set(int a, int b, int c) {
+	void set(double a, double b, double c) {
 		this->a = a;
 		this->b = b;
 		this->c = c;
 	}
 
-	void message() const {
+	void message() {
 		cout << "You entered: A = " << a << ", B = " << b << ", C = " << c << endl;
-		if (a == 0) { //checking correctness
+		if (a == 0.0) { //checking correctness
 			cout << "It isn't a quadratic equation" << endl;
 			exit(-1);
 		}
 	}
+
+	void print_roots() {
+		cout << "Roots: ";
+		for (int i = 0; i < size; i++) {
+			cout << roots[i] << " ";
+		}
+		cout << endl;
+	}
+
+	void solve_equation() {
+		double const D = b * b - 4 * a * c;
+		if (D > 0.0) {
+			roots[0] = (-b + sqrt(D)) / (2 * a);
+			roots[1] = (-b - sqrt(D)) / (2 * a);
+			size = 2;
+		}
+		else if (D == 0.0) {
+			roots[0] = -b / (2 * a);
+			size = 1;
+		}
+		else cout << "Quadratic equation has no roots" << endl;
+	}
 };
 
-void solve_equation(quadratic_equation equation) {
-	double const D = equation.b * equation.b - 4 * equation.a * equation.c;
-	if (D > 0) {
-		double x1 = (-equation.b + sqrt(D)) / (2 * equation.a);
-		double x2 = (-equation.b - sqrt(D)) / (2 * equation.a);
-		cout << "Firsr root: " << x1 << ", Second root: " << x2 << endl;
-	}
-	else if (D <= numeric_limits<double>::epsilon()) {
-		double x = -equation.b / (2 * equation.a);
-		cout << "Single root: " << x << endl;
-	}
-	else cout << "Quadratic equation has no roots" << endl;
-}
 
 int main(void) {
 	while (1) {
 		quadratic_equation equation;
 		equation.message();
 
-		solve_equation(equation);
+		equation.solve_equation();
+		equation.print_roots();
 	}
 
 	return 0;
